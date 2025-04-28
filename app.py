c3fa4814918f6516fd0f0a3ab8795ba864828f50
SalmanWithData <salmanwithdata@gmail.com>
2025-04-28 12:35:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask
-
+app = Flask(__name__)
 @app.route("/")
 def hello():
-  return "This is my flask web app page"
+  return "This is my Flask web app page"
 
 if __name__ == "__main__":
   app.run(debug = True)
